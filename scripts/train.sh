9a33709a34e08a07a8351d6df3a3a6e72c8e7603
3omni <31892476+margaritageleta@users.noreply.github.com>
2021-02-11 20:35:35 +0100

    update batch script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+sbatch <<EOT
 #!/bin/sh
+#SBATCH --job-name=PixInWav-$1
+#SBATCH -p gpi.compute
+#SBATCH -c 4
+#SBATCH --gres=gpu:1,gpumem:16G
+#SBATCH --mem=80G
+#SBATCH --time=23:59:59
+#SBATCH -o logs/exp_$1.log
+#SBATCH -e logs/exp_$1.err
 
-#SBATCH --job-name=train_stct
-#SBATCH --output=.logs/train_stct_%j.out
-#SBATCH --output=.logs/train_stct_%j.err
-#SBATCH --nodes=1
-
-srun --gres=gpu:1 -p gpi.develop --time 01:59:00 --mem 10G python ../src/model.py
+echo "Executing experiment $1"
+python3 src/trainer.py
+echo "Success!"
+EOT
