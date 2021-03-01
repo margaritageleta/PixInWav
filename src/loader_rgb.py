7f0d6ba66a0a3ab7528f651430765b184a6c5f45
punti05 <cristina.punti05@gmail.com>
2021-03-01 19:08:36 +0000

    RGB files

---
+++ src/loader_rgb.py
@@ -0,0 +1,164 @@
+import os
+import re
+import torch
+import random
+import pathlib
+import torchaudio
+import numpy as np
+import glob as glob
+from PIL import Image
+from torch.utils.data import DataLoader
+
+from pystct import sdct
+import matplotlib.pyplot as plt
+
+MY_FOLDER = os.environ.get('USER_PATH')
+DATA_FOLDER = os.environ.get('IMAGE_PATH')
+AUDIO_FOLDER = f'{MY_FOLDER}/data/FSDnoisy18k.audio_'
+MY_DATA_FOLDER = f'{MY_FOLDER}/data'
+
+class ImageProcessor():
+    def __init__(self, image_path):
+        self.image = Image.open(image_path).convert('RGB')
+
+    def crop(self, proportion = 2 ** 6):
+        nx, ny = self.image.size
+        n = min(nx, ny)
+        left = top = n / proportion
+        right = bottom = (proportion - 1) * n / proportion
+        self.image = self.image.crop((left, top, right, bottom))
+
+    def scale(self, n = 256):
+        self.image = self.image.resize((n, n), Image.ANTIALIAS)
+
+    def normalize(self):
+        self.image = np.array(self.image).astype('float') / 255.0
+
+    def forward(self):
+        self.crop()
+        self.scale()
+        self.normalize()
+        return self.image
+
+class AudioProcessor():
+    def __init__(self, audio_path):
+        self.sound, self.sr = torchaudio.load(audio_path)
+        # Corresponds to 1.5 seconds approximately
+        self._limit = 2 ** 16 + 2 ** 11 - 1
+        self._frame_length = 2 ** 12
+        self._frame_step = 2 ** 6 - 2
+
+    def forward(self):
+        # Get the samples dimension
+        sound = self.sound[0]
+        # Create a temporary array
+        tmp = torch.zeros([self._limit, ]).normal_(mean = 0, std = 0.005)
+        # Cut the audio on limit
+        if sound.numel() < self._limit:
+            tmp[:sound.numel()] = sound[:]
+        else:
+            i = random.randint(0, len(sound) - self._limit)
+            tmp[:] = sound[i:i + self._limit]
+        sound_stct = sdct(tmp.numpy().astype(np.float32),
+                          frame_length = self._frame_length,
+                          frame_step = self._frame_step)
+        return sound_stct
+
+class StegoDataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 image_root: str,
+                 audio_root: str,
+                 folder: str,
+                 mappings: dict,
+                 image_extension: str = "JPEG",
+                 audio_extension: str = "wav"):
+
+        # self._image_data_path = pathlib.Path(image_root) / folder
+        self._image_data_path = pathlib.Path(image_root) / 'train'
+        self._audio_data_path = pathlib.Path(f'{audio_root}{folder}')
+        self._MAX_LIMIT = 10000 if folder == 'train' else 900
+        self._MAX_AUDIO_LIMIT = 17584 if folder == 'train' else 946
+
+        print(f'IMAGE DATA LOCATED AT: {self._image_data_path}')
+        print(f'AUDIO DATA LOCATED AT: {self._audio_data_path}')
+
+        self.image_extension = image_extension
+        self.audio_extension = audio_extension
+        self._index = 0
+        self._indices = []
+        self._audios = []
+
+        test_i = 0
+        for key in mappings.keys():
+            for img in glob.glob(f'{self._image_data_path}/{key}/*.{self.image_extension}'):
+                test_i += 1
+                if folder == 'train' or (folder == 'test' and test_i > self._MAX_LIMIT):
+                    self._indices.append((key, re.search(r'(?<=_)\d+', img).group()))
+                    self._index += 1
+                if self._index == self._MAX_LIMIT:
+                    break
+            if self._index == self._MAX_LIMIT:
+                break
+
+        _aux_index = 0
+        for audio_path in glob.glob(f'{self._audio_data_path}/*.{self.audio_extension}'):
+            self._audios.append(audio_path)
+            _aux_index += 1
+            if _aux_index == self._MAX_AUDIO_LIMIT: break
+        random.shuffle(self._audios)
+
+        print('Set up done')
+
+    def __len__(self):
+        return self._index
+
+    def __getitem__(self, index):
+        key = self._indices[index][0]
+        indexer = self._indices[index][1]
+        rand_indexer = random.randint(0, self._MAX_AUDIO_LIMIT - 1)
+
+        img_path = glob.glob(f'{self._image_data_path}/{key}/{key}_{indexer}.{self.image_extension}')[0]
+        # print(f'{rand_indexer} < {self._MAX_AUDIO_LIMIT}')
+        audio_path = self._audios[rand_indexer]
+
+        img = np.asarray(ImageProcessor(img_path).forward()).astype('float64')
+        sound_stct = AudioProcessor(audio_path).forward()
+
+        return (img, sound_stct)
+
+def loader(set = 'train'):
+    print('Preparing')
+    mappings = {}
+    with open(f'{MY_DATA_FOLDER}/mappings.txt') as f:
+        for line in f:
+            (key, i, img) = line.split()
+            mappings[key] = img
+
+    dataset = StegoDataset(image_root=DATA_FOLDER,
+                           audio_root=AUDIO_FOLDER,
+                           folder=set,
+                           mappings=mappings)
+    print('Dataset prepared')
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=1,
+                                             shuffle=True,
+                                             num_workers=4,
+                                             pin_memory=True)
+    print('Data loaded ++')
+    return dataloader
+
+if __name__ == '__main__':
+
+    #train_loader = loader(set = 'train')
+    test_loader = loader(set = 'test')
+
+    #for i, batch in enumerate(train_loader):
+    #    print(f'Batch {i}, shape image {batch[0].shape}, shape audio {batch[1].shape}')
+    #    if i == 1: break
+    print(len(test_loader))
+    for i, batch in enumerate(test_loader):
+        print(f'Batch {i}, shape image {batch[0].shape}, shape audio {batch[1].shape}')
+        # if i == 1: break
+
+    print(len(train_loader.dataset))
+    print(len(test_loader.dataset))
---
+++ src/trainer_rgb.py
@@ -0,0 +1,341 @@
+import os
+import gc
+import time
+import torch
+import wandb
+import argparse
+import numpy as np
+import torch.nn as nn
+from loader_rgb import loader
+import torch.optim as optim
+from umodel_rgb import StegoUNet
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from pystct import isdct
+from losses import ssim, SNR, StegoLoss
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--beta', 
+                        type=float, 
+                        default=0.25, 
+                        metavar='DOUBLE',
+                        help='Beta hyperparameter'
+                    )
+parser.add_argument('--lr', 
+                        type=float, 
+                        default=0.001, 
+                        metavar='DOUBLE',
+                        help='Learning rate hyperparameter'
+                    )
+parser.add_argument('--experiment', 
+                        type=int, 
+                        default=0, 
+                        metavar='INT',
+                        help='Number of experiment'
+                    )
+parser.add_argument('--summary', 
+                        type=str, 
+                        default=None, 
+                        metavar='STRING',
+                        help='Summary to be shown in wandb'
+                    )
+
+# assert(True == False)
+
+def save_checkpoint(state, is_best, filename=os.path.join(os.environ.get('USER_PATH'),'outputs/checkpoints/checkpoint.pt')):
+	 """Save checkpoint if a new best is achieved"""
+	 if is_best:
+		 print ("=> Saving a new best model")
+		 print(f'SAVING TO: {filename}')
+		 torch.save(state, filename)  # save checkpoint
+	 else:
+		 print ("=> Loss did not improve")
+
+def compare_images(s, r):
+	s = s.permute(0,2,3,1).detach().numpy().squeeze(0)
+	r = r.permute(0,2,3,1).detach().numpy().squeeze(0)
+
+	fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+	ax[0].imshow(s)
+	ax[1].imshow(r)
+	ax[0].set_title('Secret image')
+	ax[1].set_title('Revealed image')
+	ax[0].axis('off')
+	ax[1].axis('off')
+	plt.close('all')
+
+	return fig
+
+def viz2paper(s, r, cv, ct):
+    s = s.permute(0,2,3,1).detach().numpy().squeeze(0)
+    r = r.permute(0,2,3,1).detach().numpy().squeeze(0)
+    cv = cv.detach().numpy().squeeze(0).squeeze(0)
+    ct = ct.detach().numpy().squeeze(0).squeeze(0)
+    
+
+    fig, ax = plt.subplots(2, 2, figsize=(12, 10))
+    ax[0,0].imshow(s)
+    ax[1,0].imshow(r)
+    ax[0,0].set_title('Secret image')
+    ax[1,0].set_title('Revealed image')
+    ax[0,0].axis('off')
+    ax[1,0].axis('off')
+    
+    img1 = ax[0,1].imshow(np.abs(cv)[:,], origin = 'upper', aspect = 'auto', cmap=plt.cm.get_cmap("jet"))
+    ax[0,1].set_title('Cover STCT magnitude spectrum')
+    img2 = ax[1,1].imshow(np.abs(ct)[:,], origin = 'upper', aspect = 'auto', cmap=plt.cm.get_cmap("jet"))
+    ax[1,1].set_title('Container STCT magnitude spectrum')
+    
+    ax[0,1].set_xlabel('Time [n]')
+    ax[0,1].set_ylabel('Frequency')
+    ax[1,1].set_xlabel('Time [n]')
+    ax[1,1].set_ylabel('Frequency')
+    
+    plt.colorbar(img1, ax=ax[0,1])
+    plt.colorbar(img2, ax=ax[1,1])
+    plt.close('all')
+    
+    return fig
+
+
+def train(model, tr_loader, vd_loader, beta, lr, epochs=5, prev_epoch = None, prev_i = None, summary=None, slide=50, experiment=0):
+
+	wandb.init(project='PixInWav')
+	if summary is not None:
+		wandb.run.name = summary
+		wandb.run.save()
+	wandb.watch(model)
+
+	device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	print(f'Using device: {device}')
+
+	if torch.cuda.device_count() > 1:
+  		print("Let's use", torch.cuda.device_count(), "GPUs!")
+  		model = nn.DataParallel(model)
+
+	model.to(device)
+
+	# Set to training mode
+	model.train()
+
+	# This is the number of parameters used in the model
+	num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+	print(f'Number of model parameters: {num_params}')
+
+	optimizer = optim.Adam(model.parameters(), lr=lr)
+
+	ini = time.time()
+	best_loss = np.inf
+
+	for epoch in range(epochs):
+
+		if prev_epoch != None and epoch < prev_epoch - 1: continue
+
+		train_loss, train_loss_cover, train_loss_secret, snr, ssim_secret = [], [], [], [], []
+
+		for i, data in enumerate(tr_loader):
+
+			if prev_i != None and i < prev_i - 1: continue
+
+			secrets, covers = data[0].to(device), data[1].to(device)
+			secrets = secrets.permute(0, 3, 1, 2).type(torch.cuda.FloatTensor)
+			covers = covers.unsqueeze(1)
+
+			optimizer.zero_grad()
+
+			containers, revealed = model(secrets, covers)
+
+			loss, loss_cover, loss_secret = StegoLoss(secrets, covers, containers, revealed, beta, mse=False)
+			snr_audio = SNR(covers.cpu(), containers.cpu())
+			ssim_image = ssim(secrets, revealed)
+
+			loss.backward()
+			optimizer.step()
+
+			train_loss.append(loss.detach().item())
+			train_loss_cover.append(loss_cover.detach().item())
+			train_loss_secret.append(loss_secret.detach().item())
+			snr.append(snr_audio)
+			ssim_secret.append(ssim_image.detach().item())
+
+			avg_train_loss = np.mean(train_loss[-slide:])
+			avg_train_loss_cover = np.mean(train_loss_cover[-slide:])
+			avg_train_loss_secret = np.mean(train_loss_secret[-slide:])
+			avg_snr = np.mean(snr[-slide:])
+			avg_ssim = np.mean(ssim_secret[-slide:])
+
+			print(f'(#{i})[{np.round(time.time()-ini,2)}s] Train Loss {loss.detach().item()}, MSE audio {loss_cover.detach().item()}, SSIM {loss_secret.detach().item()}, SNR {snr_audio}, SSIM {ssim_image.detach().item()}')
+
+			# Log train average loss to wandb
+			wandb.log({
+				'tr_i_loss': avg_train_loss,
+				'tr_i_cover_loss': avg_train_loss_cover,
+				'tr_i_secret_loss': avg_train_loss_secret,
+				'SNR': avg_snr,
+				'SSIM': avg_ssim,
+			})
+
+			# Log images
+			if (i % 50 == 0) and (i != 0):
+				validate(model, vd_loader, beta, tr_i=i, epoch=epoch)
+
+				is_best = bool(avg_train_loss < best_loss)
+				# Save checkpoint if is a new best
+				save_checkpoint({
+					'epoch': epoch + 1,
+					'state_dict': model.state_dict(),
+					'best_loss': best_loss,
+					'beta': beta,
+					'lr': lr,
+				}, is_best=is_best, filename=os.path.join(os.environ.get('USER_PATH'), f'outputs/checkpoints/checkpoint_run_{experiment}.pt'))
+		
+		print (f'Epoch [{epoch + 1}/{epochs}], Average_loss: {avg_train_loss}, Average_loss_cover: {avg_train_loss_cover}, Average_loss_secret: {avg_train_loss_secret}, Average SNR: {avg_snr}, Average SSIM: {avg_ssim}')
+
+		# Log train average loss to wandb
+		wandb.log({
+			'tr_loss': avg_train_loss,
+			'tr_cover_loss': avg_train_loss_cover,
+			'tr_secret_loss': avg_train_loss_secret,
+		})
+		
+		is_best = bool(avg_train_loss < best_loss)
+		best_loss = min(avg_train_loss, best_loss)
+
+		# Save checkpoint if is a new best
+		save_checkpoint({
+			'epoch': epoch + 1,
+			'state_dict': model.state_dict(),
+			'best_loss': best_loss,
+			'beta': beta,
+			'lr': lr,
+		}, is_best=is_best, filename=os.path.join(os.environ.get('USER_PATH'), f'outputs/checkpoints/checkpoint_run_{experiment}.pt'))
+
+	print(f"Training took {time.time() - ini} seconds")
+	torch.save(model.state_dict(), os.path.join(os.environ.get('USER_PATH'), f'outputs/models/final_run_{experiment}.pt'))
+	return model, avg_train_loss
+
+def validate(model, vd_loader, beta, epoch=None, tr_i=None, verbose=False):
+
+	device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	print(f'Using device: {device}')
+
+	model.to(device)
+
+	# Set to evaluation mode
+	model.eval()
+	loss = 0
+
+	valid_loss, valid_loss_cover, valid_loss_secret, valid_snr, valid_ssim = [], [], [], [], []
+	vd_datalen = len(vd_loader)
+	
+	iniv = time.time()
+	with torch.no_grad():
+		print('Validating current model...')
+		for i, data in enumerate(vd_loader):
+
+			secrets, covers = data[0].to(device), data[1].to(device)
+			secrets = secrets.permute(0, 3, 1, 2).type(torch.cuda.FloatTensor)
+			covers = covers.unsqueeze(1)
+
+			containers, revealed = model(secrets, covers)
+
+			if i == 0:
+				fig = viz2paper(secrets.cpu(), revealed.cpu(), covers.cpu(), containers.cpu())
+				wandb.log({f"Revelation at epoch {epoch}, vd iteration {tr_i}": fig})
+
+			loss, loss_cover, loss_secret = StegoLoss(secrets, covers, containers, revealed, beta, mse=False)
+			vd_snr_audio = SNR(covers.cpu(), containers.cpu())
+			ssim_image = ssim(secrets, revealed)
+
+			valid_loss.append(loss.detach().item())
+			valid_loss_cover.append(loss_cover.detach().item())
+			valid_loss_secret.append(loss_secret.detach().item())
+			valid_snr.append(vd_snr_audio)
+			valid_ssim.append(ssim_image.detach().item())
+
+			print(f'(#{i})[{np.round(time.time()-iniv,2)}s] Valid Loss {loss.detach().item()}, cover_error {loss_cover.detach().item()}, secret_error {loss_secret.detach().item()}, SNR {vd_snr_audio}, SSIM {ssim_image.detach().item()}')
+
+			if i >= 2: break
+			#if i >= vd_datalen: break
+
+		avg_valid_loss = np.mean(valid_loss)
+		avg_valid_loss_cover = np.mean(valid_loss_cover)
+		avg_valid_loss_secret = np.mean(valid_loss_secret)
+		avg_valid_snr = np.mean(valid_snr)
+		avg_valid_ssim = np.mean(valid_ssim)
+
+		wandb.log({
+			'vd_loss': avg_valid_loss,
+			'vd_cover_loss': avg_valid_loss_cover,
+			'vd_secret_loss': avg_valid_loss_secret,
+			'vd_SNR': avg_valid_snr,
+			'vd_SSIM': avg_valid_ssim,
+		})
+		print(f"Validation took {time.time() - iniv} seconds")
+
+	del valid_loss
+	del valid_loss_cover
+	del valid_loss_secret
+	del valid_snr
+	del valid_ssim
+	gc.collect()
+			
+
+if __name__ == '__main__':
+
+	args = parser.parse_args()
+
+	train_loader = loader(set = 'train')
+	test_loader = loader(set = 'test')
+
+	# chk = torch.load(f'{MY_FOLDER}/checkpoints/checkpoint_run2_1_901.pt', map_location='cpu')
+	model = StegoUNet()
+	# model.load_state_dict(chk['state_dict'])
+
+	#train(train_loader, beta = 0.3, lr = 0.001, epochs = 5, prev_epoch = chk['epoch'], prev_i = chk['i'])
+	
+	train(
+		model=model, 
+		tr_loader=train_loader, 
+		vd_loader=test_loader, 
+		beta=float(args.beta), 
+		lr=float(args.lr), 
+		epochs=20, 
+		slide=15,
+		prev_epoch=None,  
+		prev_i=None,
+		summary=args.summary,
+		experiment=int(args.experiment)
+	)
+
+
+'''
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	train_loader = loader(set = 'train')
+	test_loader = loader(set = 'test')
+	device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")	
+	print(f'Using device: {device}')
+	chk = torch.load(os.path.join(os.environ.get('USER_PATH'), f'checkpoints/checkpoint_run_{int(args.experiment)}.pt'), map_location='cpu')
+	model = StegoUNet()
+	model = nn.DataParallel(model)
+	model.load_state_dict(chk['state_dict'])
+	model.to(device)
+	print('Checkpoint loaded ++')
+	#train(train_loader, beta = 0.3, lr = 0.001, epochs = 5, prev_epoch = chk['epoch'], prev_i = chk['i'])
+	train(
+		model=model, 
+		tr_loader=train_loader, 
+		vd_loader=test_loader, 
+		beta=float(args.beta), 
+		lr=float(args.lr), 
+		epochs=15, 
+		slide=15,
+		prev_epoch=chk['epoch'], 
+		prev_i=None,
+		summary=args.summary,
+		experiment=int(args.experiment)
+	)
+
+'''
