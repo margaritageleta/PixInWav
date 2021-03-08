df41c49484308a5881bc30478e90f512601d3485
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-08 21:12:56 +0000

    Instant experiments

---
+++ scripts/instant.sh
@@ -0,0 +1,12 @@
+# Usage:  ./instant.sh [experiment_number] [beta] [learning_rate]
+# Sample: ./instant.sh 13 0.05 0.001  
+
+echo "Executing experiment $1"
+srun -u -c 1 -p gpi.develop --time 02:00:00 --mem 32G --gres=gpu:2,gpumem:12G python3 ../src/trainer_rgb.py --beta $2 \
+--lr $3 \
+--summary "RGB unshuffle DTW + noise, beta=$2, lr=$3" \
+--experiment $1 \
+--add_noise True
+--noise_kind "salt&pepper"
+--noise_amplitude 0.001
+echo "Success!"
