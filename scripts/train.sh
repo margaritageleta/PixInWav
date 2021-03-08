c759d2e996defc5dac132d378cee9c6901b2be3f
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-08 21:38:54 +0000

    Add noise arguments

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@ sbatch <<EOT
 #!/bin/sh
 #SBATCH --job-name=$1PixInWav
 #SBATCH -p gpi.compute
-#SBATCH -c 4
+#SBATCH -c 1
 #SBATCH --gres=gpu:2,gpumem:12G
 #SBATCH --mem=32G
 #SBATCH --time=23:59:59
@@ -11,6 +11,12 @@ sbatch <<EOT
 #SBATCH -e $OUT_PATH/logs/pixinwav_$1.err
 
 echo "Executing experiment $1"
-python3 ../src/trainer_rgb.py --beta $2 --lr $3 --summary "RGB unshuffle DTW + noise, beta=$2, lr=$3" --experiment $1
+python3 ../src/trainer_rgb.py --beta $2 \
+--lr $3 \
+--summary "Run $1: Salt&Pepper (0.1), beta=$2, lr=$3" \
+--experiment $1 \
+--add_noise True
+--noise_kind "salt"
+--noise_amplitude 0.1
 echo "Success!"
 EOT
