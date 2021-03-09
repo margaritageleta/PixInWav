55c3cbf840bc97a219d3795d1df7991ce478e218
margaritageleta <noticiasmundiales99@gmail.com>
2021-03-09 18:15:21 +0000

    Combine loader L and RGB

--- src/loader.py
+++ src/loader.py
@@ -9,7 +9,7 @@ import glob as glob
 from PIL import Image
 from torch.utils.data import DataLoader
 
-from pystct import sdct
+from pystct import sdct_torch, isdct_torch
 import matplotlib.pyplot as plt
 
 MY_FOLDER = os.environ.get('USER_PATH')
@@ -18,8 +18,17 @@ AUDIO_FOLDER = f'{MY_FOLDER}/data/FSDnoisy18k.audio_'
 MY_DATA_FOLDER = f'{MY_FOLDER}/data'
 
 class ImageProcessor():
-    def __init__(self, image_path):
-        self.image = Image.open(image_path).convert('L')
+    """
+    Function to preprocess the images from the custom 
+    dataset. It includes a series of transformations:
+
+    - At __init__ we convert the image to the desired [colorspace].
+    - Crop function crops the image to the desired [proportion].
+    - Scale scales the images to desired size [n]x[n].
+    - Normalize performs the normalization of the channels.
+    """
+    def __init__(self, image_path, colorspace='RGB'):
+        self.image = Image.open(image_path).convert(colorspace)
 
     def crop(self, proportion = 2 ** 6):
         nx, ny = self.image.size
@@ -41,10 +50,16 @@ class ImageProcessor():
         return self.image
 
 class AudioProcessor():
+    """
+    Function to preprocess the audios from the custom 
+    dataset. We set the [_limit] in terms of samples,
+    the [_frame_length] and [_frame_step] of the STCT
+    transform. 
+    """
     def __init__(self, audio_path):
         self.sound, self.sr = torchaudio.load(audio_path)
         # Corresponds to 1.5 seconds approximately
-        self._limit = 2 ** 16 + 2 ** 11 - 1
+        self._limit = 67522 # 2 ** 16 + 2 ** 11 - 2 ** 6 + 2
         self._frame_length = 2 ** 12
         self._frame_step = 2 ** 6 - 2
 
@@ -59,25 +74,47 @@ class AudioProcessor():
         else:
             i = random.randint(0, len(sound) - self._limit)
             tmp[:] = sound[i:i + self._limit]
-        sound_stct = sdct(tmp.numpy().astype(np.float32),
-                          frame_length = self._frame_length,
-                          frame_step = self._frame_step)
+        sound_stct = sdct_torch(
+            tmp.type(torch.float32),
+            frame_length = self._frame_length,
+            frame_step = self._frame_step
+        )
         return sound_stct
 
 class StegoDataset(torch.utils.data.Dataset):
-    def __init__(self,
-                 image_root: str,
-                 audio_root: str,
-                 folder: str,
-                 mappings: dict,
-                 image_extension: str = "JPEG",
-                 audio_extension: str = "wav"):
+    """
+    Custom datasets pairing images with spectrograms.
+    - [image_root] defines the path to read the images from.
+    - [audio_root] defines the path to read the audio clips from.
+    - [folder] can be either [train] or [test].
+    - [mappings] is the dictionary containing a descriptive name for 
+    the images from ImageNet. It is used to index the different
+    subdirectories.
+    - [rgb] is a boolean that indicated whether we are using color (RGB)
+    images or black and white ones (B&W).
+    - [image_extension] defines the extension of the image files. 
+    By default it is set to JPEG.
+    - [audio_extension] defines the extension of the audio files. 
+    By default it is set to WAV.
+    """
+
+    def __init__(
+        self,
+        image_root: str,
+        audio_root: str,
+        folder: str,
+        mappings: dict,
+        rgb: bool = True,
+        image_extension: str = "JPEG",
+        audio_extension: str = "wav"
+    ):
 
         # self._image_data_path = pathlib.Path(image_root) / folder
         self._image_data_path = pathlib.Path(image_root) / 'train'
         self._audio_data_path = pathlib.Path(f'{audio_root}{folder}')
         self._MAX_LIMIT = 10000 if folder == 'train' else 900
         self._MAX_AUDIO_LIMIT = 17584 if folder == 'train' else 946
+        self._colorspace = 'RGB' if rgb else 'L'
 
         print(f'IMAGE DATA LOCATED AT: {self._image_data_path}')
         print(f'AUDIO DATA LOCATED AT: {self._audio_data_path}')
@@ -118,47 +155,43 @@ class StegoDataset(torch.utils.data.Dataset):
         rand_indexer = random.randint(0, self._MAX_AUDIO_LIMIT - 1)
 
         img_path = glob.glob(f'{self._image_data_path}/{key}/{key}_{indexer}.{self.image_extension}')[0]
-        # print(f'{rand_indexer} < {self._MAX_AUDIO_LIMIT}')
         audio_path = self._audios[rand_indexer]
 
-        img = np.asarray(ImageProcessor(img_path).forward()).astype('float64')
+        img = np.asarray(ImageProcessor(image_path=img_path, colorspace=self._colorspace).forward()).astype('float64')
         sound_stct = AudioProcessor(audio_path).forward()
 
         return (img, sound_stct)
 
-def loader(set = 'train'):
-    print('Preparing')
+def loader(set='train', rgb=True):
+    """
+    Prepares the custom dataloader.
+    - [set] defines the set type. Can be either [train] or [test].
+    - [rgb] is a boolean that indicated whether we are using color (RGB)
+    images or black and white ones (B&W).
+    """
+    print('Preparing dataset...')
     mappings = {}
     with open(f'{MY_DATA_FOLDER}/mappings.txt') as f:
         for line in f:
             (key, i, img) = line.split()
             mappings[key] = img
 
-    dataset = StegoDataset(image_root=DATA_FOLDER,
-                           audio_root=AUDIO_FOLDER,
-                           folder=set,
-                           mappings=mappings)
-    print('Dataset prepared')
-    dataloader = torch.utils.data.DataLoader(dataset,
-                                             batch_size=1,
-                                             shuffle=True,
-                                             num_workers=4,
-                                             pin_memory=True)
+    dataset = StegoDataset(
+        image_root=DATA_FOLDER,
+        audio_root=AUDIO_FOLDER,
+        folder=set,
+        mappings=mappings,
+        rgb=rgb
+    )
+
+    print('Dataset prepared.')
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=1,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True
+    )
+
     print('Data loaded ++')
     return dataloader
-
-if __name__ == '__main__':
-
-    #train_loader = loader(set = 'train')
-    test_loader = loader(set = 'test')
-
-    #for i, batch in enumerate(train_loader):
-    #    print(f'Batch {i}, shape image {batch[0].shape}, shape audio {batch[1].shape}')
-    #    if i == 1: break
-    print(len(test_loader))
-    for i, batch in enumerate(test_loader):
-        print(f'Batch {i}, shape image {batch[0].shape}, shape audio {batch[1].shape}')
-        # if i == 1: break
-
-    print(len(train_loader.dataset))
-    print(len(test_loader.dataset))
